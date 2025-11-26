cbea964165f0f8a0cf4daffa7a47a60b82ddc91b
WoodIsDaniel <danielspriensma21@gmail.com>
2025-11-26 14:07:04 +1000

    v1.8.95

---
+++ src/main/java/org/woodisdaniel/immersivetechnology/common/ModCreativeModeTabs.java
@@ -0,0 +1,34 @@
+package org.woodisdaniel.immersivetechnology.common;
+
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredRegister;
+import org.woodisdaniel.immersivetechnology.common.block.ModBlocks;
+import org.woodisdaniel.immersivetechnology.common.item.ModItems;
+import org.woodisdaniel.immersivetechnology.ImmersiveTechnology;
+
+import java.util.function.Supplier;
+
+public class ModCreativeModeTabs {
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TAB =
+            DeferredRegister.create(Registries.CREATIVE_MODE_TAB, ImmersiveTechnology.MOD_ID);
+
+    public static final Supplier<CreativeModeTab> IMMERSIVETECHNOLOGY_MISC_TAB = CREATIVE_MODE_TAB.register("immersivetechnology",
+            () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModItems.SALT.get()))
+                    .title(Component.translatable("creativetab.immersivetechnology.immersivetechnology_misc_items"))
+                    .displayItems((itemDisplayParameters, output) -> {
+
+                        //output.accept(Items.WOODEN_SWORD);
+                        output.accept(ModBlocks.SALT_BLOCK);
+                        output.accept(ModItems.SALT);;
+
+                    }).build());
+
+    public static void register(IEventBus eventBus) {
+        CREATIVE_MODE_TAB.register(eventBus);
+    }
+}
